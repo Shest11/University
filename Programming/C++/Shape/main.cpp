f401d80b1a5d51d7c58cce4bd47f9325fb0a9450
ShestM <danilasestakov8@gmail.com>
2026-05-18 21:08:02 +0800

    Fixed main.cpp

--- Programming/C++/Shape/main.cpp
+++ Programming/C++/Shape/main.cpp
@@ -1,45 +1,32 @@
 #include "shapes.h"
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 int main() {
     const Point pt1(3, 4);
-    Circle circ(pt1, 4);
-    circ.name();
-    cout << "Площадь: " << circ.calc_area() << endl;
-    cout << "Периметр: " << circ.calc_perimetr() << endl;
-
-    cout << "" << endl;
-
-    Ellipse ellipse(pt1, 5, 4);
-    ellipse.name();
-    cout << "Площадь: " << ellipse.calc_area() << endl;
-    cout << "Периметр: " << ellipse.calc_perimetr() << endl;
-
-    cout << "" << endl;
-
     const Point pt2(5, 1);
-    Rectangle rect(pt1, pt2);
-    rect.name();
-    cout << "Площадь: " << rect.calc_area() << endl;
-    cout << "Периметр: " << rect.calc_perimetr() << endl;
-
-    cout << "" << endl;
-
     const Point pt3(6, 8);
-    Triangle trian(pt1, pt2, pt3);
-    trian.name();
-    cout << "Площадь: " << trian.calc_area() << endl;
-    cout << "Периметр: " << trian.calc_perimetr() << endl;
 
-    cout << "" << endl;
+    vector<Shape*> shapes;
 
-    string filename = "granitsy-uchastka2.txt";
-    Polygon poly(filename);
-    poly.name();
-    cout << "Площадь: " << poly.calc_area() << endl;
-    cout << "Периметр: " << poly.calc_perimetr() << endl;
+    shapes.push_back(new Circle(pt1, 4));
+    shapes.push_back(new Ellipse(pt1, 5, 4));
+    shapes.push_back(new Rectangle(pt1, pt2));
+    shapes.push_back(new Triangle(pt1, pt2, pt3));
+    shapes.push_back(new Polygon("granitsy-uchastka2.txt"));
 
-    cout << "" << endl;
+    for (size_t i = 0; i < shapes.size(); ++i) {
+        shapes[i]->name();
+        cout << "Площадь: " << shapes[i]->calc_area() << endl;
+        cout << "Периметр: " << shapes[i]->calc_perimetr() << endl;
+        cout << "" << endl;
+    }
+
+    for (size_t i = 0; i < shapes.size(); ++i) {
+        delete shapes[i];
+    }
+    shapes.clear();
+
 }
